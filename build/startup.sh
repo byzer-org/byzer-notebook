6d84633c318054b6a732dbfc23f27c9b7cc288da
jiachuan.zhu <chncaesar@gmail.com>
2022-04-27 14:17:02 +0800

    Fix syntax error

--- build/startup.sh
+++ build/startup.sh
@@ -9,7 +9,7 @@ echo "NOTEBOOK_HOME="${NOTEBOOK_HOME}
 HANG_UP_FLAG=${1:-nohup}
 if [[ "${HANG_UP_FLAG}" == "nohup" ]]; then
   echo "====================================="
-  echo "Nohup startup byzer notebook."
+  echo "Nohup startup Byzer Notebook."
   nohup java -DNOTEBOOK_HOME=${NOTEBOOK_HOME} -Dspring.config.name=application,notebook -Dspring.config.location=classpath:/,file:${NOTEBOOK_HOME}/conf/ -jar ${NOTEBOOK_HOME}/notebook-console.jar > ${NOTEBOOK_HOME}/logs/notebook.log 2>&1 &
   echo "$!" > ${pid}
   echo "success"
