f8a9270d5f4d88ec64eadc46faf591474b70d70b
WilliamZhu <allwefantasy@gmail.com>
2022-10-23 18:26:20 +0800

    update

---
+++ src/main/scala/io/kyligence/notebook/console/scalalib/hint/VisualizationHint.scala
@@ -0,0 +1,23 @@
+package io.kyligence.notebook.console.scalalib.hint
+
+import scala.collection.mutable
+
+/**
+ * 17/10/2022 WilliamZhu(allwefantasy@gmail.com)
+ */
+class VisualizationHint extends BaseHint {
+  override def rewrite(query: String, options: mutable.Map[String, String]): String = {
+    val header = _parse(query)
+    if (header.t.toLowerCase != "visualize") {
+      return query
+    }
+
+    require(header.input.isDefined, "--%input is required")
+
+    s"""
+       |!visualize ${header.input} '''
+       |${header.body}
+       |''';
+       |""".stripMargin
+  }
+}
