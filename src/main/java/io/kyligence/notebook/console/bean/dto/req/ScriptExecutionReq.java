bcb2c0ea1dfba15b697ae5a7b92045f53e719057
WilliamZhu <allwefantasy@gmail.com>
2023-01-14 13:27:15 +0800

    support base64 enc sql

--- src/main/java/io/kyligence/notebook/console/bean/dto/req/ScriptExecutionReq.java
+++ src/main/java/io/kyligence/notebook/console/bean/dto/req/ScriptExecutionReq.java
@@ -4,8 +4,10 @@ package io.kyligence.notebook.console.bean.dto.req;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.apache.commons.codec.binary.Base64;
 
 import javax.validation.constraints.NotBlank;
+import java.io.UnsupportedEncodingException;
 
 @Data
 @NoArgsConstructor
@@ -21,10 +23,24 @@ public class ScriptExecutionReq {
     @JsonProperty("cell_id")
     private Integer cellId;
 
+    @JsonProperty("encType")
+    private String encType;
+
     @JsonProperty("engine")
     private String engine;
 
     @JsonProperty("timeout")
     private Integer timeout;
+
+    public String getSql() {
+        if (encType != null && encType.equals("base64")) {
+            try {
+                return new String(Base64.decodeBase64(sql), "UTF-8");
+            } catch (UnsupportedEncodingException e) {
+                return sql;
+            }
+        }
+        return sql;
+    }
 
 }
