4afb028ba20d3666e45e4e29bdef1e21e1e1f03d
WilliamZhu <allwefantasy@gmail.com>
2023-02-21 16:45:23 +0800

    code suggest need base65

--- src/main/java/io/kyligence/notebook/console/bean/dto/req/CodeSuggestionReq.java
+++ src/main/java/io/kyligence/notebook/console/bean/dto/req/CodeSuggestionReq.java
@@ -3,8 +3,11 @@ package io.kyligence.notebook.console.bean.dto.req;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.apache.commons.codec.binary.Base64;
 import org.springframework.util.LinkedMultiValueMap;
 import org.springframework.util.MultiValueMap;
+
+import java.io.UnsupportedEncodingException;
 
 @Data
 @NoArgsConstructor
@@ -12,6 +15,9 @@ public class CodeSuggestionReq {
 
     @JsonProperty("sql")
     private String sql;
+
+    @JsonProperty("encType")
+    private String encType;
 
     @JsonProperty("lineNum")
     private Long lineNum;
@@ -21,5 +27,16 @@ public class CodeSuggestionReq {
 
     @JsonProperty("isDebug")
     private Boolean isDebug;
+
+    public String getSql() {
+        if (encType != null && encType.equals("base64")) {
+            try {
+                return new String(Base64.decodeBase64(sql), "UTF-8");
+            } catch (UnsupportedEncodingException e) {
+                return sql;
+            }
+        }
+        return sql;
+    }
 
 }
