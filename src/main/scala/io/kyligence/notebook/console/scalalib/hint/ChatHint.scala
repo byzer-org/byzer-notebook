1d53b73d3c05d4dabb0a455c2a5ec98098cc3fdd
WilliamZhu <allwefantasy@gmail.com>
2023-06-16 22:29:39 +0800

    update

--- src/main/scala/io/kyligence/notebook/console/scalalib/hint/ChatHint.scala
+++ src/main/scala/io/kyligence/notebook/console/scalalib/hint/ChatHint.scala
@@ -1,4 +1,5 @@
 package io.kyligence.notebook.console.scalalib.hint
+
 import org.apache.commons.lang3.StringEscapeUtils
 import scala.collection.mutable
 
@@ -12,23 +13,40 @@ class ChatHint extends BaseHint {
       return query
     }
     val model = header.params("model")
-    val temperature = header.params.getOrElse("temperature","0.1")
-    val userRole = header.params.getOrElse("user_role","")
-    val assistantRole = header.params.getOrElse("assistant_role","")
-    val systemMsg = header.params.getOrElse("system_msg","You are a helpful assistant. Think it over and answer the user's question correctly.")
-    val output = header.output.get
+    val temperature = header.params.getOrElse("temperature", "0.1")
+    val userRole = header.params.getOrElse("user_role", "")
+    val assistantRole = header.params.getOrElse("assistant_role", "")
+    val systemMsg = header.params.getOrElse("system_msg", "")
+    val outputOpt = header.output
+    val inputOpt = header.input
+
 
     val instruction = StringEscapeUtils.escapeJava(header.body);
 
-    s"""
-       |select ${model}(llm_param(map(
-       |    "system_msg",'${systemMsg}',
-       |    "instruction",'${instruction}',
-       |    "temperature","${temperature}",
-       |    "user_role","${userRole}",
-       |    "assistant_role","${assistantRole}"
-       |))) as q as ${output};
-       |
-       |""".stripMargin
+    if (inputOpt.isDefined) {
+      s"""
+         |select ${model}(llm_param(map(
+         |    "system_msg",'${systemMsg}',
+         |    "instruction",'${instruction}',
+         |    "temperature","${temperature}",
+         |    "user_role","${userRole}",
+         |    "assistant_role","${assistantRole}"
+         |))) as q from ${inputOpt.get} as ${outputOpt.getOrElse("output")};
+         |
+         |""".stripMargin
+    } else {
+      s"""
+         |select ${model}(llm_param(map(
+         |    "system_msg",'${systemMsg}',
+         |    "instruction",'${instruction}',
+         |    "temperature","${temperature}",
+         |    "user_role","${userRole}",
+         |    "assistant_role","${assistantRole}"
+         |))) as q as ${outputOpt.getOrElse("output")};
+         |
+         |""".stripMargin
+    }
+
+
   }
 }
