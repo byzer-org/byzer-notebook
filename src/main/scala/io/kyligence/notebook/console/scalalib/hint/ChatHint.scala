e910adde813cdd6baf5e8dae50153143826a09e8
WilliamZhu <allwefantasy@gmail.com>
2023-06-17 20:11:51 +0800

    update

--- src/main/scala/io/kyligence/notebook/console/scalalib/hint/ChatHint.scala
+++ src/main/scala/io/kyligence/notebook/console/scalalib/hint/ChatHint.scala
@@ -13,35 +13,34 @@ class ChatHint extends BaseHint {
       return query
     }
     val model = header.params("model")
-    val temperature = header.params.getOrElse("temperature", "0.1")
-    val userRole = header.params.getOrElse("user_role", "")
-    val assistantRole = header.params.getOrElse("assistant_role", "")
-    val systemMsg = header.params.getOrElse("system_msg", "")
+    //    val temperature = header.params.getOrElse("temperature", "0.1")
+    //    val userRole = header.params.getOrElse("user_role", "")
+    //    val assistantRole = header.params.getOrElse("assistant_role", "")
+    //    val systemMsg = header.params.getOrElse("system_msg", "")
+   val params =  header.params.map { case (k, v) =>
+      s""""${k}",'${v}'"""
+    }.mkString(",\n")
+
     val outputOpt = header.output
     val inputOpt = header.input
 
 
-    val instruction = StringEscapeUtils.escapeJava(header.body);
+    val instruction = StringEscapeUtils.escapeJava(header.body.trim);
 
     if (inputOpt.isDefined) {
       s"""
          |select  chat(llm_stack(q,llm_param(map(
          |
          |    "instruction",'${instruction}',
-         |    "temperature","${temperature}",
-         |    "user_role","${userRole}",
-         |    "assistant_role","${assistantRole}"
+         |    ${params}
          |)))) as q from ${inputOpt.get} as ${outputOpt.getOrElse("output")};
          |
          |""".stripMargin
     } else {
       s"""
          |select ${model}(llm_param(map(
-         |    "system_msg",'${systemMsg}',
          |    "instruction",'${instruction}',
-         |    "temperature","${temperature}",
-         |    "user_role","${userRole}",
-         |    "assistant_role","${assistantRole}"
+         |    ${params}
          |))) as q as ${outputOpt.getOrElse("output")};
          |
          |""".stripMargin
