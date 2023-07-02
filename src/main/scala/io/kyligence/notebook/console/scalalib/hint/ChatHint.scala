093ce998c4dc6a19ce0a62ff363143b0cbfc7f80
WilliamZhu <allwefantasy@gmail.com>
2023-07-02 15:09:22 +0800

    upate

--- src/main/scala/io/kyligence/notebook/console/scalalib/hint/ChatHint.scala
+++ src/main/scala/io/kyligence/notebook/console/scalalib/hint/ChatHint.scala
@@ -17,8 +17,13 @@ class ChatHint extends BaseHint {
     //    val userRole = header.params.getOrElse("user_role", "")
     //    val assistantRole = header.params.getOrElse("assistant_role", "")
     //    val systemMsg = header.params.getOrElse("system_msg", "")
+    // --%image=image_field_name
+    // --%image_table=image_table_name
    val params =  header.params.map { case (k, v) =>
-      s""""${k}",'${v}'"""
+      Array(k,v) match {
+        case Array("image",image_field_name) => s""""image",${image_field_name}"""
+        case _ => s""""${k}",'${v}'"""
+      }
     }.mkString(",\n")
 
     val outputOpt = header.output
@@ -26,11 +31,46 @@ class ChatHint extends BaseHint {
 
     val format = header.params.getOrElse("format", "html")
 
+    if(header.params.contains("image")){
+      val image = header.params("image")
+      val imageSQL =
+        s"""
+           |select concat('<img src="data:image/png;base64,', '${image}', '" />') as content, "html" as mime from ${outputOpt.getOrElse("output")} as byzerllm_format_table;
+           |""".stripMargin
+      return imageSQL
+    }
+
 //    val tempTableName = UUID.randomUUID().toString .replaceAll("-", "")
 
+    val preHTML =  StringEscapeUtils.escapeJava(
+      """
+        |<!DOCTYPE html>
+        |<html>
+        |<head>
+        |  <style>
+        |    .custom-pre {
+        |      height: 200px; /* Set the desired height */
+        |      overflow: auto; /* Add scrollbars if the content exceeds the height */
+        |      white-space: pre-wrap; /* Enable line wrapping */
+        |    }
+        |  </style>
+        |</head>
+        |<body>
+        |  <pre class="custom-pre">
+        |""".stripMargin  )
+
+    val endPreHTML =
+      StringEscapeUtils.escapeJava( """
+        |</pre>
+        |</body>
+        |</html>
+        |""".stripMargin )
+
     val formatSQL = if (format == "html"){
-      s"""select concat("<pre>",llm_response_predict(q),"</pre>") as content, "${format}" as mime from ${outputOpt.getOrElse("output")} as byzerllm_format_table;"""
+      s"""select concat("${preHTML}",llm_response_predict(q),"${endPreHTML}") as content, "${format}" as mime from ${outputOpt.getOrElse("output")} as byzerllm_format_table;"""
     } else ""
+
+
 
     val instruction = StringEscapeUtils.escapeJava(header.body.trim);
 
