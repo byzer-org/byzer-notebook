a039d21ee2fb2910f76eeab0a7ec691ac7bfccc5
jiachuan Zhu <chncaesar@gmail.com>
2023-07-30 11:17:03 +0800

    Fix $NOTEBOOK_HOME error

--- build/startup.sh
+++ build/startup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-dir=$(cd -P -- "$(dirname -- "${BASH_SOURCE[0]}")" && pwd -P)
+dir=$(cd -P -- "$(dirname -- "${BASH_SOURCE[0]}")/.." && pwd -P)
 pid=${dir}/pid
 
 export NOTEBOOK_HOME=${dir}
@@ -10,11 +10,11 @@ HANG_UP_FLAG=${1:-nohup}
 if [[ "${HANG_UP_FLAG}" == "nohup" ]]; then
   echo "====================================="
   echo "Nohup startup Byzer Notebook."
-  nohup java -DNOTEBOOK_HOME=${NOTEBOOK_HOME} -Dspring.config.name=application,notebook -Dspring.config.location=classpath:/,file:${NOTEBOOK_HOME}/conf/ -jar ${NOTEBOOK_HOME}/notebook-console.jar > ${NOTEBOOK_HOME}/logs/notebook.log 2>&1 &
+  nohup java -DNOTEBOOK_HOME=${NOTEBOOK_HOME} -Dspring.config.name=application,notebook -Dspring.config.location=classpath:/,file:${NOTEBOOK_HOME}/conf/ -jar ${NOTEBOOK_HOME}/lib/notebook-console.jar > ${NOTEBOOK_HOME}/logs/notebook.log 2>&1 &
   echo "$!" > ${pid}
   echo "success"
 elif [[ "${HANG_UP_FLAG}" == "hangup" ]];then
   echo "====================================="
   echo "startup byzer notebook."
-  java -DNOTEBOOK_HOME=${NOTEBOOK_HOME} -Dspring.config.name=application,notebook -Dspring.config.location=classpath:/,file:${NOTEBOOK_HOME}/conf/ -jar ${NOTEBOOK_HOME}/notebook-console.jar
+  java -DNOTEBOOK_HOME=${NOTEBOOK_HOME} -Dspring.config.name=application,notebook -Dspring.config.location=classpath:/,file:${NOTEBOOK_HOME}/conf/ -jar ${NOTEBOOK_HOME}/lib/notebook-console.jar
 fi
